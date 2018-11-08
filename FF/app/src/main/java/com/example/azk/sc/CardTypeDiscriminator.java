a27444a00bb7ff90ca51d6be0b7b9ac4ee40c66e
jacklin <jack098000@gmail.com>
2018-11-08 17:39:18 +0800

    update basic system

---
+++ FF/app/src/main/java/com/example/azk/sc/CardTypeDiscriminator.java
@@ -0,0 +1,15 @@
+package com.example.azk.sc;
+
+import java.util.ArrayList;
+
+public class CardTypeDiscriminator {
+    private ArrayList<String> ccCard;
+    private ArrayList<String> playerCard;
+
+    CardTypeDiscriminator(ArrayList<String> cc, ArrayList<String> pc){
+        ccCard = cc;
+        pc = playerCard;
+    }
+
+
+}
--- FF/app/src/main/java/com/example/azk/sc/MainActivity.java
+++ FF/app/src/main/java/com/example/azk/sc/MainActivity.java
@@ -7,13 +7,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button btnFold,btnCheck,btnRaise;
     private ImageView ivCC1,ivCC2,ivCC3,ivCC4,ivCC5,ivHC1,ivHC2 ;
     private TextView tv1,tv2,tv3;
-
-
+    private TextView tvTurn, tvCheck;
+    private DeckController deckController;
+    private int turn_number;
+    private boolean all_check;
+    private ArrayList<String> playerCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //初始化
-    protected void  init(){
+    protected void init(){
         //按钮们
         btnFold= (Button) findViewById(R.id.b_fold);
         btnRaise= (Button) findViewById(R.id.b_raise);
@@ -40,26 +45,97 @@ public class MainActivity extends AppCompatActivity {
         tv1=(TextView) findViewById(R.id.tv_tm2);
         tv2=(TextView) findViewById(R.id.tv_tm4);
         tv3=(TextView) findViewById(R.id.tv_tm5);
-
+        //其餘TextView們
+        tvTurn = (TextView) findViewById(R.id.tv_turn);
+        tvCheck = (TextView) findViewById(R.id.tv_check);
+        //牌組控制器
+        deckController = new DeckController();
+        playerCard = new ArrayList<>();
+        resetGame();
     }
 
+    public void resetGame(){
+        int imageResource = getResources().getIdentifier("@drawable/jb", null, getPackageName());
+        ivCC1.setImageResource(imageResource);
+        ivCC2.setImageResource(imageResource);
+        ivCC3.setImageResource(imageResource);
+        ivCC4.setImageResource(imageResource);
+        ivCC5.setImageResource(imageResource);
+        String p1Card = deckController.drawCard();
+        String connect = "@drawable/" + p1Card;
+        imageResource = getResources().getIdentifier(connect, null, getPackageName());
+        ivHC1.setImageResource(imageResource);
+        String p2Card = deckController.drawCard();
+        connect = "@drawable/" + p2Card;
+        imageResource = getResources().getIdentifier(connect, null, getPackageName());
+        ivHC2.setImageResource(imageResource);
+        turn_number = 0;
+        all_check = false;
+        tvTurn.setText(String.valueOf(turn_number));
+        tvCheck.setText(String.valueOf(all_check));
+        playerCard.add(p1Card);
+        playerCard.add(p2Card);
+    }
 
     //下注按钮ButtonRaise[android:onClick="ButtonRaise"]
     public void ButtonRaise(View view) {
         ButtonRaise MyButtonRaise=new ButtonRaise();
-        MyButtonRaise.getContext(this);
-        MyButtonRaise.showNormalDialog();
+        //MyButtonRaise.getContext(this);
+        //MyButtonRaise.showNormalDialog();
+        turn_number += 1;
+        all_check = true;
+        tvCheck.setText(String.valueOf(all_check));
     }
     //弃牌按钮ButtonFold[android:onClick="ButtonRaise"]
     public void ButtonFold(View view) {
         ButtonFold MyButtonFold=new ButtonFold();
+        deckController.resetDeck();
+        resetGame();
     }
     //弃牌按钮ButtonCheck[android:onClick="ButtonRaise"]
     public void ButtonCheck(View view) {
         ButtonCheck MyButtonCheck=new ButtonCheck();
+        String cc1Card, cc2Card, cc3Card, cc4Card, cc5Card;
+        ArrayList<String> ccCard = new ArrayList<>();
+        if(all_check) {
+            if (turn_number == 1) {
+                cc1Card = deckController.drawCard();
+                cc2Card = deckController.drawCard();
+                cc3Card = deckController.drawCard();
+                String connect = "@drawable/" + cc1Card;
+                int imageResource = getResources().getIdentifier(connect, null, getPackageName());
+                ivCC1.setImageResource(imageResource);
+                connect = "@drawable/" + cc2Card;
+                imageResource = getResources().getIdentifier(connect, null, getPackageName());
+                ivCC2.setImageResource(imageResource);
+                connect = "@drawable/" + cc3Card;
+                imageResource = getResources().getIdentifier(connect, null, getPackageName());
+                ivCC3.setImageResource(imageResource);
+                ccCard.add(cc1Card);
+                ccCard.add(cc2Card);
+                ccCard.add(cc3Card);
+            } else if (turn_number == 2) {
+                cc4Card = deckController.drawCard();
+                String connect = "@drawable/" + cc4Card;
+                int imageResource = getResources().getIdentifier(connect, null, getPackageName());
+                ivCC4.setImageResource(imageResource);
+                ccCard.add(cc4Card);
+            } else if (turn_number == 3) {
+                cc5Card = deckController.drawCard();
+                String connect = "@drawable/" + cc5Card;
+                int imageResource = getResources().getIdentifier(connect, null, getPackageName());
+                ivCC5.setImageResource(imageResource);
+                ccCard.add(cc5Card);
+                CardTypeDiscriminator ctd = new CardTypeDiscriminator(ccCard,playerCard);
+
+            } else {
+                resetGame();
+            }
+        }
+        all_check = false;
+        tvTurn.setText(String.valueOf(turn_number));
+        tvCheck.setText(String.valueOf(all_check));
     }
-
-
 
     }
 
